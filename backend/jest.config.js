f9df62f93ba75911a2ecb63509d6c60390d7fd14
Bilal Asif <bilalasif1024@gmail.com>
2026-09-01 23:18:53 +0500

    Update jest.config.js

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -5,7 +5,16 @@ module.exports = {
   testMatch: ['**/*.test.ts'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
-
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov'],
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/dist/',
+    '/coverage/'
+  ],
   setupFiles: ['<rootDir>/src/test/setup.ts'],
   globals: {
     'ts-jest': {
