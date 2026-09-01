21f8f88c7e05aadbdf98294a7046a4641fdbd44d
Bilal Asif <bilalasif1024@gmail.com>
2026-09-01 23:18:47 +0500

    Update jest.config.js to remove coverage settings
    Removed coverage-related configurations from Jest.

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -5,16 +5,7 @@ module.exports = {
   testMatch: ['**/*.test.ts'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  collectCoverage: true,
-  coverageDirectory: 'coverage',
-  coverageReporters: ['text', 'lcov'],
-  coveragePathIgnorePatterns: [
-    '/node_modules/',
-    '/dist/',
-    '/coverage/'
-  ],
+
   setupFiles: ['<rootDir>/src/test/setup.ts'],
   globals: {
     'ts-jest': {
